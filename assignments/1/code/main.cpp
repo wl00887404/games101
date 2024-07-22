26f48b0e2cab72cfd3b3bc4e29d9141d3e321adc
Max He <max.loves.frontend@gmail.com>
2024-07-22 16:36:21 +0800

    發現 NDC 的角度轉弧度寫錯，超級尷尬

--- assignments/1/code/main.cpp
+++ assignments/1/code/main.cpp
@@ -58,7 +58,7 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
    * width = height * aspect_ratio
    * length = zNear - zFar
    */
-  float eye_fov_radian = eye_fov * 180 / MY_PI;
+  float eye_fov_radian = eye_fov / 180 * MY_PI;
   float height = abs(zNear) * tan(eye_fov_radian) * 2;
   float width = height * aspect_ratio;
   float length = zNear - zFar;
@@ -136,7 +136,7 @@ int main(int argc, const char** argv) {
 
     r.set_model(get_model_matrix(angle));
     r.set_view(get_view_matrix(eye_pos));
-    r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+    r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
     r.draw(pos_id, ind_id, rst::Primitive::Triangle);
     cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
@@ -152,8 +152,7 @@ int main(int argc, const char** argv) {
 
     r.set_model(get_model_matrix(angle));
     r.set_view(get_view_matrix(eye_pos));
-    // MAX: 幹怎麼正的
-    r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+    r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
     r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
